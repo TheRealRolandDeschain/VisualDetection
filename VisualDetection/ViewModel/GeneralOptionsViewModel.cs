cc8b9490d38a9dbc32f3cc8370247054a7acc49d
Roland Deschain <johannes.zellinger@gmx.at>
2018-10-26 19:50:41 +0200

    Added WMI query to get list of available cameras

--- VisualDetection/ViewModel/GeneralOptionsViewModel.cs
+++ VisualDetection/ViewModel/GeneralOptionsViewModel.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Management;
 
 
 namespace VisualDetection.ViewModel
@@ -20,9 +18,13 @@ namespace VisualDetection.ViewModel
         #region Private Properties
         private List<string> availableCameras;
         private int selectedCameraIndex;
+        private int idleAfterFrameCalculationMS;
         #endregion
 
         #region Public Prooperties
+        /// <summary>
+        /// The list of all available cameras on the system. 
+        /// </summary>
         public List<string> AvailableCameras
         {
             get
@@ -35,6 +37,9 @@ namespace VisualDetection.ViewModel
             }
         }
 
+        /// <summary>
+        /// The index of the currently selected camera
+        /// </summary>
         public int SelectedCameraIndex
         {
             get
@@ -47,12 +52,49 @@ namespace VisualDetection.ViewModel
                 SetProperty(ref selectedCameraIndex, value);
             }
         }
+
+        /// <summary>
+        /// Time the programm waits after each frame
+        /// </summary>
+        public int IdleAfterFrameCalculationMS
+        {
+            get
+            {
+                return idleAfterFrameCalculationMS;
+            }
+            set
+            {
+                idleAfterFrameCalculationMS = value;
+                SetProperty(ref idleAfterFrameCalculationMS, value);
+            }
+        }
+
         #endregion
 
         #region Private Methods
+        /// <summary>
+        /// uses WMI to get all available cameras and add them to the list
+        /// </summary>
         public void GetAvailableCameraList()
         {
-            AvailableCameras = new List<string> { "Camera 1", "Camera 2", "Camera 3", "Camera 4", "Camera 5", "Camera 6", "Camera 7", "Camera 8", "Camera 9" };
+            AvailableCameras = new List<string>();
+            string wmiQuery = string.Format("SELECT * FROM Win32_PnPSignedDriver");
+
+            Console.WriteLine("Query: {0}", wmiQuery);
+
+            ManagementObjectSearcher searcher = new ManagementObjectSearcher(wmiQuery);
+            ManagementObjectCollection retObjectCollection = searcher.Get();
+
+            foreach (var WmiObject in retObjectCollection)
+            {
+                    if (WmiObject["DeviceClass"] != null && WmiObject["DeviceClass"].ToString().Equals("IMAGE"))
+                    {
+                        AvailableCameras.Add(WmiObject["DeviceName"].ToString());
+                    }
+                
+            }
+
+            
             SelectedCameraIndex = 0;
         }
         #endregion
