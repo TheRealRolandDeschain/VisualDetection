adf52a1d60d3e4026cddf987219cf4323ad3f005
Roland Deschain <johannes.zellinger@gmx.at>
2018-11-22 14:25:24 +0100

    Prerelease code

--- VisualDetection/App.xaml.cs
+++ VisualDetection/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using Squirrel;
+using System;
+using System.Windows;
 
 namespace VisualDetection
 {
@@ -6,6 +8,35 @@ namespace VisualDetection
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
-  {
-  }
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            CheckForUpdates();
+        }
+
+        #region Private Methods
+        /// <summary>
+        /// Checks for new Updates on StartUp and let's the user choose to restart
+        /// in order to get the new update immediately. 
+        /// </summary>
+        /// <returns></returns>
+        private void CheckForUpdates()
+        {
+            try
+            {
+                using (var manager = new UpdateManager(@"C:/Temp"))
+                {
+                    manager.UpdateApp();
+                }
+            }
+            catch(Exception e)
+            {
+                MessageBox.Show(
+                    $"No comparable program files found in update path! \n\n Error: {e.Message}"
+                    );
+            }
+        }
+        #endregion
+    }
 }
