b1b8217b9f4b7a8248869de333fc8ea9982bc1e3
Roland Deschain <johannes.zellinger@gmx.at>
2018-11-22 15:18:57 +0100

    -added squirrel -removed testing area from outputview

--- VisualDetection/App.xaml.cs
+++ VisualDetection/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Squirrel;
+﻿
+using Squirrel;
 using System;
 using System.Windows;
 
@@ -9,34 +10,10 @@ namespace VisualDetection
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            CheckForUpdates();
-        }
+        #region Protected Methods
+        #endregion
 
         #region Private Methods
-        /// <summary>
-        /// Checks for new Updates on StartUp and let's the user choose to restart
-        /// in order to get the new update immediately. 
-        /// </summary>
-        /// <returns></returns>
-        private void CheckForUpdates()
-        {
-            try
-            {
-                using (var manager = new UpdateManager(@"C:/Temp"))
-                {
-                    manager.UpdateApp();
-                }
-            }
-            catch(Exception e)
-            {
-                MessageBox.Show(
-                    $"No comparable program files found in update path! \n\n Error: {e.Message}"
-                    );
-            }
-        }
         #endregion
     }
 }
--- VisualDetection/View/OutputView.xaml.cs
+++ VisualDetection/View/OutputView.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows.Controls;
 
 namespace VisualDetection.View
 {
@@ -25,39 +12,5 @@ namespace VisualDetection.View
             InitializeComponent();
         }
 
-
-        private void Test_MouseDown(object sender, MouseButtonEventArgs e)
-        {
-
-            if (e.LeftButton == MouseButtonState.Pressed)
-            {
-                Test.Background = Brushes.Red;
-            }
-            else if (e.RightButton == MouseButtonState.Pressed)
-            {
-                Test.Background = Brushes.Blue;
-            }
-            else if (e.MiddleButton == MouseButtonState.Pressed)
-            {
-                Test.Background = Brushes.Green;
-            }
-        }
-
-        private void Test_MouseUp(object sender, MouseButtonEventArgs e)
-        {
-            Test.Background = Brushes.White;
-        }
-
-        private void Test_MouseWheel(object sender, MouseWheelEventArgs e)
-        {
-            if(e.Delta > 0)
-            {
-                Test.Background = Brushes.Yellow;
-            }
-            else
-            {
-                Test.Background = Brushes.Brown;
-            }
-        }
     }
 }
