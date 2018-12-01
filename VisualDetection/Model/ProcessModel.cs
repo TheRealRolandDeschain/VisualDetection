ff0c7abe7c263e6189bb947c6adb842873942cff
Roland Deschain <johannes.zellinger@gmx.at>
2018-12-01 14:35:40 +0100

    commit for safety before creating datagrid template

---
+++ VisualDetection/Model/ProcessModel.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VisualDetection.ViewModel;
+
+namespace VisualDetection.Model
+{
+    public class ProcessModel : ViewModelBase
+    {
+        #region Constructor
+        //Creates a new process via a path
+        public ProcessModel(string path)
+        {
+            ProcessName = Path.GetFileNameWithoutExtension(path);
+            processPath = path;
+            instances = new List<Process>();
+            NrOfInstancesRunning = 3;
+        }
+        #endregion
+
+        #region Private Properties
+        private string processName;
+        private uint nrOfInstancesRunning;
+        private string processPath;
+        private List<Process> instances;
+        private bool isTriggeredLeft;
+        private bool isTriggeredRight;
+        private bool isTriggeredOpenClose;
+        #endregion
+
+        #region Public Properties
+        /// <summary>
+        /// The name of this Process
+        /// </summary>
+        public string ProcessName
+        {
+            get { return processName; }
+            set
+            {
+                if (processName != value)
+                {
+                    SetProperty(ref processName, value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// The number of instances of the current process
+        /// that are running
+        /// </summary>
+        public uint NrOfInstancesRunning
+        {
+            get { return nrOfInstancesRunning; }
+            set
+            {
+                if (nrOfInstancesRunning != value)
+                {
+                    SetProperty(ref nrOfInstancesRunning, value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Determines if the process is triggerd by the left trigger
+        /// that are running
+        /// </summary>
+        public bool IsTriggeredLeft
+        {
+            get { return isTriggeredLeft; }
+            set
+            {
+                if (isTriggeredLeft != value)
+                {
+                    SetProperty(ref isTriggeredLeft, value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Determines if the process is triggerd by the right trigger
+        /// that are running
+        /// </summary>
+        public bool IsTriggeredRight
+        {
+            get { return isTriggeredRight; }
+            set
+            {
+                if (isTriggeredRight != value)
+                {
+                    SetProperty(ref isTriggeredRight, value);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Determines if the process is triggerd so that it 
+        /// opens with the left and closes with the right trigger
+        /// that are running
+        /// </summary>
+        public bool IsTriggeredOpenClose
+        {
+            get { return isTriggeredOpenClose; }
+            set
+            {
+                if (isTriggeredOpenClose != value)
+                {
+                    SetProperty(ref isTriggeredOpenClose, value);
+                }
+            }
+        }
+        #endregion
+
+        #region Private Methods
+        #endregion
+
+        #region Public Methods
+        #endregion
+    }
+}
--- VisualDetection/ViewModel/OpenSoftwareOptionsViewModel.cs
+++ VisualDetection/ViewModel/OpenSoftwareOptionsViewModel.cs
@@ -1,11 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using VisualDetection.Util;
 using VisualDetection.Interfaces;
 using VisualDetection.Model;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 
 namespace VisualDetection.ViewModel
 {
@@ -19,13 +17,35 @@ namespace VisualDetection.ViewModel
         #endregion
 
         #region Private Properties
+        private int indexOfSelectedProcess;
         #endregion
 
         #region Public Properties
+
+        /// <summary>
+        /// The index of the currently selected index
+        /// </summary>
+        public ObservableCollection<ProcessModel> ListOfProcesses { get; set; }
+
         /// <summary>
         /// Title of this Option
         /// </summary>
         public string OptionTitle { get; private set; }
+
+        /// <summary>
+        /// The index of the currently selected index
+        /// </summary>
+        public int IndexOfSelectedProcess
+        {
+            get { return indexOfSelectedProcess; }
+            set
+            {
+                if (indexOfSelectedProcess != value)
+                {
+                    SetProperty(ref indexOfSelectedProcess, value);
+                }
+            }
+        }
         #endregion
 
         #region Private Methods
@@ -35,6 +55,9 @@ namespace VisualDetection.ViewModel
         private void SetDefaultValues()
         {
             OptionTitle = GenDefString.OpenExternalSoftwareTitle;
+            ListOfProcesses = new ObservableCollection<ProcessModel>();
+            ListOfProcesses.Add(new ProcessModel(@"C:\Program Files (x86)\Notepad++\notepad++.exe"));
+            ListOfProcesses.Add(new ProcessModel(@"C:\Program Files\Tracker Software\PDF Editor\PDFXEdit.exe"));
         }
         #endregion
 
